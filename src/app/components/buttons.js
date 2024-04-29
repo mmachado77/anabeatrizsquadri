eda7d7d3a8ba66947d0907107733760bc44223d7
Matheus Machado <matheusmachado77@gmail.com>
2024-04-29 00:55:29 -0300

    Responsividade revertida

--- src/app/components/buttons.js
+++ src/app/components/buttons.js
@@ -6,11 +6,11 @@ import { Button } from "primereact/button";
 export default function Buttons() {
 
     return (
-        <div className='mt-5 mr-5 flex justify-between : mr-0 flex flex-col sm'>
+        <div className="mt-0 lg:mt-5 mr-0 lg:mr-5 flex justify-between">
         <a href="https://wa.me/5551996367799" target="_blank" rel="noopener noreferrer">
-            <Button className='w-full sm' label="Mande uma Mensagem" icon="pi pi-whatsapp" severity='success'/>
+            <Button className='' label="Mande uma Mensagem" icon="pi pi-whatsapp" severity='success'/>
         </a>
         <label className='text-black font-bold text-4xl'>51 99636-7799</label>
-    </div>
+        </div>
     );
 }
