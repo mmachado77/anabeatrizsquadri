3e40b7de7746372e58731d0d437864f66460b665
Matheus Machado <matheusmachado77@gmail.com>
2024-04-30 12:03:24 -0300

    implementacao speed insight

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import "tailwindcss/utilities.css";
 import "primereact/resources/themes/lara-light-green/theme.css";
