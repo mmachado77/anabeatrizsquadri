4ba6dc27c9ffb4f6c251843f132c3de4bac8c67d
Matheus Machado <matheusmachado77@gmail.com>
2024-05-02 21:02:33 -0300

    GA componentezado

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,16 +2,12 @@ import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import GoogleAnalytics from "./utilities/GoogleAnalytics";
 import "tailwindcss/utilities.css";
 import "primereact/resources/themes/lara-light-green/theme.css";
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import './globals.css';
-import ReactGA from "react-ga4";
-
-
-const TRACKING_ID = "G-5RCVLW04QE"
-ReactGA.initialize(TRACKING_ID);
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        
+      <GoogleAnalytics /> 
       <link rel="icon" href="favicon.svg"/>
       </head>
       <body className={inter.className}>{children}
---
+++ src/app/utilities/google-analytics.js
@@ -0,0 +1,22 @@
+import ReactGA from "react-ga4";
+
+const initializeGA = () => {
+  // Replace with your Measurement ID
+  // It ideally comes from an environment variable
+  ReactGA.initialize("G-5RCVLW04QE");
+  // Don't forget to remove the console.log() statements
+  // when you are done
+};
+
+const trackGAEvent = (category, action, label) => {
+  console.log("GA event:", category, ":", action, ":", label);
+  // Send GA4 Event
+  ReactGA.event({
+    category: category,
+    action: action,
+    label: label,
+  });
+};
+
+export default initializeGA;
+export { initializeGA, trackGAEvent };
